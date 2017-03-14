f1841a8ea5cd3f7cbd6e08d62cc73886f921a8c5
Manjula <Manjula>
2017-03-15 01:20:01 +0530

    module 4 assigntment

--- src/menudata/categories.controller.js
+++ src/menudata/categories.controller.js
@@ -4,9 +4,9 @@
   angular.module('Data')
   .controller('CategoriesController',CategoriesController);
 
-  CategoriesController.$inject = ['MenuDataService','items'];
+  CategoriesController.$inject = ['items'];
 
-  function CategoriesController(MenuDataService,items){
+  function CategoriesController(items){
     var categoriesList = this;
     categoriesList.items = items.data;
   }
